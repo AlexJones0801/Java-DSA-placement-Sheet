8c9fd14efc51144de8229789b722fb0b105f8d8d
Shivansh Kurmi <shivanshkurmi2@gmail.com>
2025-05-15 13:53:14 +0530

    Q5 moving all neg number to beginning and positive to end

---
+++ Q5_negFirstPosEnd.java
@@ -0,0 +1,28 @@
+// Q5. Move all negative numbers to beginning and positive to end
+
+public class Q5_negFirstPosEnd {
+
+    public static void main(String[] args) {
+        int[] arr = { -1, 2, -3, 4, -5, 6, -7,3 };
+        int left = 0, right=arr.length-1;
+
+        while (left <= right) {
+            if(arr[left] < 0) { //if element is < 0 do not change anything as it's in the right place
+                left++;
+            } else if(arr[right]>0){ // if element is > 0 do not change anything as it's in the right place
+                right--;
+            } else {  // if arr[left] is positive and arr[right] is negative swap them
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
