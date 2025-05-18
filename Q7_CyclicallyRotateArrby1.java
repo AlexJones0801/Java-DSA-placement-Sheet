b2ff460e39f3238b4d8c49b7331d786eeaa516e1
Shivansh Kurmi <shivanshkurmi2@gmail.com>
2025-05-18 15:39:12 +0530

    Q7. Cyclically rotate an array by one

---
+++ Q7_CyclicallyRotateArrby1.java
@@ -0,0 +1,23 @@
+// Q7. Cyclically rotate an array by one
+
+public class Q7_CyclicallyRotateArrby1 {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,8,12};
+        int lastelem = arr[arr.length-1];
+
+
+        for(int i = arr.length-1 ; i >= 0;i--) {
+            if (i>0) {
+                arr[i] = arr[i-1];
+            } else {
+                arr[0] = lastelem;
+            }
+        }
+        
+        System.out.println("One Cyclical rotate array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
