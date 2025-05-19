5dd23be5af3210fd972b6022c99e8dd732b57992
Shivansh Kurmi <shivanshkurmi2@gmail.com>
2025-05-19 14:04:28 +0530

    Q9. Check if array is sorted and rotated

---
+++ Q9_ifArrisSortedandRotated.java
@@ -0,0 +1,33 @@
+// Q9. Check if array is sorted and rotated
+
+import java.util.Scanner;
+
+public class Q9_ifArrisSortedandRotated {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter Elements of Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int next = (i + 1) % arr.length;
+            if (arr[i] > arr[next]) {
+                count++;
+            }
+
+        }
+
+        if (count == 1)
+            System.out.println("Sorted and Rotated");
+        else
+            System.out.println("Not Sorted and Rotated");
+    }
+}
