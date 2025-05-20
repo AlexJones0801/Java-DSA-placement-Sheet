153a2dfd9c2311eedf600ec54287a84e2d0e2d66
Shivansh Kurmi <129269469+AlexJones0801@users.noreply.github.com>
2025-05-20 21:16:53 +0530

    Update Q7_CyclicallyRotateArrby1.java

--- Q7_CyclicallyRotateArrby1.java
+++ Q7_CyclicallyRotateArrby1.java
@@ -7,7 +7,7 @@ public class Q7_CyclicallyRotateArrby1 {
         int lastelem = arr[arr.length-1];
 
 
-        for(int i = arr.length-1 ; i >= 0;i--) {
+        for(int i = arr.length-1 ; i > 0;i--) {
                 arr[i] = arr[i-1];
         }
         arr[0] = lastelem;
