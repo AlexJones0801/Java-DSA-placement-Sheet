30530a06774db1e152feb96bd85a2f142110064e
Shivansh Kurmi <shivanshkurmi2@gmail.com>
2025-05-24 17:47:10 +0530

    12. Count the number of occurrences of an element

---
+++ Q12_CountNoOfOccOfAnElem.java
@@ -0,0 +1,16 @@
+// 12. Count the number of occurrences of an element
+
+public class Q12_CountNoOfOccOfAnElem {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 1, 5, 3, 3, 5 };
+        int target = 3, count = 0; // Target element whose occurrence we will find
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Target element " + target + " occurrence is " + count);
+    }
+}
