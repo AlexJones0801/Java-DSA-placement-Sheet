9b92b7921478b145579d2c27bf230e09b4ec6b63
Shivansh Kurmi <shivanshkurmi2@gmail.com>
2025-05-26 10:53:14 +0530

    14. Subarray with given sum (Two pointer / Sliding window)

---
+++ Q14_SubArrWithGiveSum.java
@@ -0,0 +1,31 @@
+// 14. Subarray with given sum (Two pointer / Sliding window)
+
+public class Q14_SubArrWithGiveSum {
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 20, 3, 10, 5 };
+        int targetSum = 33, start = 0, currentSum = 0;
+        boolean found = false;
+
+        for (int end = 0; end < arr.length; end++) {
+            currentSum += arr[end];
+
+            // Shrink the window from the left if currentSum > targetSum
+            while (currentSum > targetSum && start < end) {
+                currentSum -= arr[start];
+                start++;
+            }
+
+            // Checking if current Window has the target sum
+            if (currentSum == targetSum) {
+                System.out.println("Subarray found from index " + start + " to " + end);
+                found = true;
+                break;
+            }
+
+        }
+
+        if (!found) {
+            System.out.println("No subarray with given sum found.");
+        }
+    }
+}
