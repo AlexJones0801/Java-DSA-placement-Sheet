511bc0a399859011bd8a9661ab6230995c6d95d2
Shivansh Kurmi <shivanshkurmi2@gmail.com>
2025-05-28 21:04:27 +0530

    16. Find duplicate number in array

---
+++ Q16_DupliNoArr.java
@@ -0,0 +1,19 @@
+// 16. Find duplicate number in array
+
+import java.util.HashSet;
+
+public class Q16_DupliNoArr {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 2, 3, 5, 3 };
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i : arr) {
+            if (set.contains(i)) {
+                System.out.println("Duplicate: " + i);
+            } else {
+                set.add(i);
+            }
+        }
+    }
+}
